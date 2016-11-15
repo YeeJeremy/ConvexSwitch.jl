59a2b4118c6db3d6f7a67778bf072c0d9799d09b
Jeremy Yee <jeremyyee@outlook.com.au>
2016-11-15 21:58:24 +1100

    added nearest neighbors approach - Mart

--- src/Mart.jl
+++ src/Mart.jl
@@ -85,5 +85,79 @@ immutable Mart
 
     ## Using nearest neighbours
     
+    function Mart(bellman::Bellman, path::Path, subsim::Array{Float64, 5},
+                  subsim_weight::Array{Float64, 1}, search::Function,
+                  grid::Array{Float64, 2})
+
+        method = "row-rearrange"
+
+        ## Extract sizes from CSS
+        pnum = bellman.pnum
+        anum = bellman.anum
+        pathnum = path.pathnum
+        subsimnum = size(subsim)[4]
+        tnum = bellman.tnum
+        fullcontrol = bellman.fullcontrol
+            
+        ## Container
+        result = zeros(Array{Float64}(tnum - 1, pnum, pathnum))
+        path_state = Array{Float64}(bellman.snum)
+        disturb_state = Array{Float64}(bellman.snum)
+
+        ## Tree
+        tgrid = transpose(grid)
+        tree = search(tgrid)
+        
+        print("Martingales: ")
+        ##  Compute martingales
+        for t = 1:(tnum - 1)
+            print(t, ".")
+            for p = 1:pathnum
+                path_state = path.sample[t, p, :]
+                for pos = 1:pnum
+                    ## Finding the average
+                    for h = 1:subsimnum
+                        disturb_state = subsim[:, :, p, h, t] * path_state
+                        nn = knn(tree, disturb_state, 1, true)[1]
+                        result[t, pos, p] += subsim_weight[h] *
+                            (bellman.value[nn, :, pos, t + 1] * disturb_state)[1]
+                    end
+                    ## Subtracting the path realization
+                    path_state = path.sample[t + 1, p, :]
+                    nn = knn(tree, path_state, 1 ,true)[1]
+                    result[t, pos, p] -=
+                        (bellman.value[nn, :, pos, t + 1] * path_state)[1]
+                end
+            end    
+        end
+
+        if fullcontrol
+            print("End.\n")
+            return new(result, method, pnum, anum, pathnum, subsimnum, tnum,
+                       fullcontrol)
+        else # partial control of positions
+            ## Container
+            result2 = Array{Float64}(tnum - 1, pnum, anum, pathnum)
+            prob_weight = Array{Float64}(pnum)            
+            ## Adjust the martingales for partial control
+            for t = 1:(tnum - 1)
+                for pos = 1:pnum
+                    for a = 1:anum
+                        prob_weight = bellman.control[pos, a, :]
+                        result2[t, pos, a, :] = transpose(prob_weight) * result[t, :, :]
+                    end
+                end
+            end
+
+            print("End.\n")
+            return new(result2, method, pnum, anum, pathnum, subsimnum, tnum,
+                       fullcontrol)
+
+        end
+        
+    end
+
+
+
 
 end
